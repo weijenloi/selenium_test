71aa49a8477ee4476ddfb082f1c36235da26499b
Loi, Wei Jen <WeiJen.Loi@experian.com>
2021-06-09 22:11:55 +0800

    setup logging

--- maintest.py
+++ maintest.py
@@ -1,9 +1,18 @@
 # http://demos.telerik.com/kendo-ui/dragdrop/index
+import os
+import socket
+import sys
+import datetime
+from loguru import logger
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 
-def main():
+from build_log import StreamToLogger, build_logger
 
+
+def main():
+    log_path = os.getcwd()
+    setup_log(log_path)
     options = ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_experimental_option("useAutomationExtension", False)
@@ -21,8 +30,18 @@ def main():
         },
     )
     driver = webdriver.Chrome(options=options, executable_path=r'.\chromedriver.exe')
-    driver.get('https://duckduckgo.com')
-    input()
+    test_page = 'https://duckduckgo.com'
+    logger.info(f'Navigate to test page {test_page}')
+    driver.get(test_page)
 
+def setup_log(log_path):
+    """Setup logger"""
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    log_file = os.path.join(log_path, "test_log_%s.log" % datetime.datetime.today().strftime('%d-%m-%Y_%H-%M-%S'))
+    sys.stdout = StreamToLogger(logger, "INFO")
+    sys.stderr = StreamToLogger(logger, "ERROR")
+    build_logger(log_file, 'DEBUG')
+    logger.info(f'Machine: {socket.gethostname()}')
 
 main()
